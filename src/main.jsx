575f0202668d80bee4032e832149d3da451fb130
Frédéric <frederic.djomakon@gmail.com>
2016-11-20 20:51:14 +0100

    skeleton up and running

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./componoents/List.jsx');
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
